39afea6b4a7788a19fc288b4e14aaba7ec48635b
y64yfdb4w7-creator <y64yfdb4w7@privaterelay.appleid.com>
2026-06-24 11:22:04 +0300

    fix(ui): render tulot_items_p in renderRightPanel (Nykyinen rytmi bug)
    Add rendering of income items in the UI.

--- js/ui2-v2.js
+++ js/ui2-v2.js
@@ -4383,6 +4383,15 @@ function renderRightPanel(snaps, latest, calc) {
         + '<span class="panel-row-val pos">+' + fmtP(tulot_yht) + '</span></div>';
     }
     // Rakenteet eriteltynä
+if (tulot_items_p.length > 0) {
+  tulot_items_p.forEach(function(t) {
+    var amt = parseFloat(t.amt_kk);
+    if (!amt) return;
+    html += '<div class="panel-row">'
+    +(t.label||'Tulorivi')+'</span>'
+    +'<span class="panel-row-val pos">+'+amt.toFixed(0)+' €</span></div>';
+  });
+}
     if (rytmi_items_p.length > 0) {
       html += '<div style="height:1px;background:var(--border);margin:5px 0;"></div>';
       rytmi_items_p.forEach(function(r) {
